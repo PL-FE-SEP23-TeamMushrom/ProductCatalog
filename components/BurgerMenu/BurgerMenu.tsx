18bc4aaa07e552029344f787e2b7482305d2f1dd
Mirosław Wandyk <barosz1111@gmail.com>
2024-02-08 15:32:57 +0100

    very tasty burger

--- components/BurgerMenu/BurgerMenu.tsx
+++ components/BurgerMenu/BurgerMenu.tsx
@@ -1,16 +1,10 @@
 import Image from "next/image";
-import Logo from "@/public/ui-kit/Logo.webp";
-import Close from "@/public/icons/Close.svg";
 import Heart from "@/public/icons/Heart.svg";
 import ShoppingBag from "@/public/icons/ShoppingBag.svg";
 
 export default function BurgerMenu() {
     return (
     <div className="menu">
-        <nav className="navigation flex justify-between">
-            <Image src={Logo} alt="logo" />
-            <Image src={Close} alt="close" />
-        </nav>
         <ul className="flex flex-col space-y-4 items-center mt-6 mb-69 text-secondary-color font-extrabold">
             <li>
                 <a href="#" className="">HOME</a>
--- components/navigation/Navbar.tsx
+++ components/navigation/Navbar.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link'
 import { usePathname } from 'next/navigation';
 import styles from './Navbar.module.css'
 import { useState } from 'react';
+import BurgerMenu from '../BurgerMenu/BurgerMenu';
 
 const Navbar = () => {
   const pathname = usePathname();
@@ -16,34 +17,37 @@ const Navbar = () => {
     };
 
   return (
-    <header className="w-full flex border-b justify-between">
-      <div className="wrapper flex items-center">
-        <Link href={'/'}>
-          <Image className='max-w-none' width={100} height={100} src={'/home.svg'} alt="home image" />
-        </Link>
-        <Link href={'/'} className={`${styles.normalLink} ${pathname === '/' ? styles.customLink : ''}`}>
-          <div>Home</div>
-        </Link>
-        <Link href={'/phones'} className={`${styles.normalLink} ${pathname === '/phones' ? styles.customLink : ''}`}>
-          <div>Phones</div>
-        </Link>
-        <Link href={'/tablets'} className={`${styles.normalLink} ${pathname === '/tablets' ? styles.customLink : ''}`}>
-          <div>Tablets</div>
-        </Link>
-        <Link href={'/accesories'} className={`${styles.normalLink} ${pathname === '/accesories' ? styles.customLink : ''}`}>
-          <div>Accesories</div>
-        </Link>
-      </div>
-      <div className='flex'>
-        <div className={`${styles.hamburgermenu} ${isActive ? styles.active : ''}`} onClick={toggleActive}>
-          <span className={`${styles.bar} ${styles.bar1}`}></span>
-          <span className={`${styles.bar} ${styles.bar2}`}></span>
-          <span className={`${styles.bar} ${styles.bar3}`}></span>
+    <>
+      <header className="w-full flex border-b justify-between">
+        <div className="wrapper flex items-center">
+          <Link href={'/'}>
+            <Image className='max-w-none' width={100} height={100} src={'/home.svg'} alt="home image" />
+          </Link>
+          <Link href={'/'} className={`${styles.normalLink} ${pathname === '/' ? styles.customLink : ''}`}>
+            <div>Home</div>
+          </Link>
+          <Link href={'/phones'} className={`${styles.normalLink} ${pathname === '/phones' ? styles.customLink : ''}`}>
+            <div>Phones</div>
+          </Link>
+          <Link href={'/tablets'} className={`${styles.normalLink} ${pathname === '/tablets' ? styles.customLink : ''}`}>
+            <div>Tablets</div>
+          </Link>
+          <Link href={'/accesories'} className={`${styles.normalLink} ${pathname === '/accesories' ? styles.customLink : ''}`}>
+            <div>Accesories</div>
+          </Link>
         </div>
-        <Image className={`${styles.normalimage} border-x border-gray-300`} width={20} height={20} src={'/icons/Heart.svg'} alt="cart image" />
-        <Image className={`${styles.normalimage}`} width={20} height={20} src={'/icons/shop.svg'} alt="cart image" />
-      </div>
-    </header>
+        <div className='flex'>
+          <div className={`${styles.hamburgermenu} ${isActive ? styles.active : ''}`} onClick={toggleActive}>
+            <span className={`${styles.bar} ${styles.bar1}`}></span>
+            <span className={`${styles.bar} ${styles.bar2}`}></span>
+            <span className={`${styles.bar} ${styles.bar3}`}></span>
+          </div>
+          <Image className={`${styles.normalimage} border-x border-gray-300`} width={20} height={20} src={'/icons/Heart.svg'} alt="cart image" />
+          <Image className={`${styles.normalimage}`} width={20} height={20} src={'/icons/shop.svg'} alt="cart image" />
+        </div>
+      </header>
+      {isActive && <BurgerMenu />}
+    </>
   )
 }
 
