53313bf3dff06fe323def85df673a23772b6004a
marcinzrodlowski <formyriadofsetups@gmail.com>
2024-02-08 15:22:27 +0100

    Pagination blueprint with mock data

---
+++ app/[phones]/page.tsx
@@ -0,0 +1,28 @@
+import Pagination from "@/components/Pagination/Pagination";
+import Card from "@/components/ProductCard/ProductCard";
+
+export default function Phones() {
+  return (
+    <>
+      <Pagination>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </Pagination>
+    </>
+  );
+}
