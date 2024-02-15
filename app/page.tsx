862544cf73f1e6d2143d0e39c91ceb67c5a96bf5
Mirosław Wandyk <131676378+Barosz30@users.noreply.github.com>
2024-02-15 15:32:22 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import getManyWithYear from "@/utils/getManyWithFind";
 export default async function Home() {
   const phones = await getManyWithYear(2022);
 
+  const db = await connectToDatabase()
   const hotPrices = await db.collection<Product>("products").find({}).toArray();
   const hotPricesSerialized = JSON.parse(
     JSON.stringify(hotPrices)
