980b980b1b75341cff80e12bea5ba50a9fe9c4e6
Monika Osowiecka <mosowiecka@users.noreply.github.com>
2024-02-15 23:20:15 +0100

    working version

--- utils/getAll.tsx
+++ utils/getAll.tsx
@@ -1,11 +1,12 @@
-
 import connectToDatabase from "@/lib/connect";
 
-export default async function getProductById( itemId: number  ) {
+export default async function getAll() {
     const db = await connectToDatabase()
     const result = await db
         .collection<Product>("products")
-        .findOne({ id: itemId });
+        .find()
+        .map(({_id, ...rest}) => (rest))
+        .toArray();
 
     return result;
 }
