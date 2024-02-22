8b085f9b23edcebdbbd66186b8f0dfc0a7aa16e1
Monika Osowiecka <mosowiecka@users.noreply.github.com>
2024-02-22 20:11:46 +0100

    added modal

--- components/Cart/Cart.tsx
+++ components/Cart/Cart.tsx
@@ -4,17 +4,21 @@ import useLocalStorage from '@/hooks/useLocalStorage';
 import { useState, useEffect, useRef, useCallback } from 'react';
 import {CartProduct} from "./CartProduct";
 import Back from '../Back/Back';
+import { Modal } from '@/components/Modal/Modal';
 
 interface CartItem {
   [productId: string]: number;
 }
 
 export default function Cart() {
+  const storage = useLocalStorage('CART');
+
   const [cart, setCart] = useState<{ product: Product, quantity: number }[]>([]);
   const productsArray: CartItem[] = useLocalStorage('CART')?.getItem();
   const stringifiedProducts = JSON.stringify(productsArray)
   const [totalCost, setTotalCost] = useState<number>(0);
   const [totalCount, setTotalCount] = useState<number>(0);
+  const [ifCheckout,setIfCheckout] =useState(false);
 
   const fetchData = useCallback(async() => {
     try {
@@ -65,9 +69,20 @@ export default function Cart() {
   }, [cart]);
 
 
-
+const checkoutButtonHandler = () => {
+  if (storage) {
+    for (const c in cart) {
+      storage.removeItemFromCart(c);
+    }
+    setCart([]);
+    setTotalCost(0);
+    setTotalCount(0);
+    setIfCheckout(true);
+  }
+}
   return (
             <div className="">
+              {ifCheckout && <Modal />}
                     <Back />
                 <h1 className="col-span-full desktop:text-5xl tablet:text-5xl mobile:text-l desktop:leading-56 tablet:leading-56 mt-4 mb-8 mobile: leading-41">Cart</h1>
                 <div className="grid grid-cols-1 auto-rows-1 gap-gap desktop:grid-cols-17 tablet:grid-cols-1 mobile:grid-cols-1">
@@ -83,7 +98,8 @@ export default function Cart() {
                         <p className="total text-2xl font-extrabold leading-41 desktop:mt-23">${totalCost}</p>
                         <p className="totalItems text-sm font-medium leading-21">Total for {totalCount} items</p>
                         <div className="h-96 mt-25 border border-transparent border-t-elements-color flex justify-center items-center">
-                            <button className="text-sm desktop:w-320 tablet:w-544 leading-21 font-bold w-320 h-48 checkoutButton bg-black text-white">Checkout</button>
+                            <button className="text-sm desktop:w-320 tablet:w-544 leading-21 font-bold w-320 h-48 checkoutButton bg-black text-white"
+                            onClick={checkoutButtonHandler}>Checkout</button>
                         </div>
                     </div>
                 </div>
